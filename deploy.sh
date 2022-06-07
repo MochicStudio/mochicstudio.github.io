ab85e554916335e60804f7ee6a158b471d93db10
andrdc <andrsruiz@gmail.com>
2022-06-07 17:56:18 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:mochicstudio/mochicstudio.github.io.git master
+cd -
